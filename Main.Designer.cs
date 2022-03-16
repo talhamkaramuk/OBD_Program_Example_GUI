30a2e042d5e63b918cd867300a7f660bcc21fc1e
Talha <talha@mail.com>
2022-03-16 13:15:37 +0300

    Side menu completed

---
+++ Main.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Sample_2
+{
+    partial class Main
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSettings = new FontAwesome.Sharp.IconButton();
+            this.btn5 = new FontAwesome.Sharp.IconButton();
+            this.btn4 = new FontAwesome.Sharp.IconButton();
+            this.btnDashboard = new FontAwesome.Sharp.IconButton();
+            this.btnSetup = new FontAwesome.Sharp.IconButton();
+            this.btnHome = new FontAwesome.Sharp.IconButton();
+            this.panelSpace = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnMenu = new FontAwesome.Sharp.IconButton();
+            this.pboxLogo = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.White;
+            this.panelMenu.Controls.Add(this.btnSettings);
+            this.panelMenu.Controls.Add(this.btn5);
+            this.panelMenu.Controls.Add(this.btn4);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Controls.Add(this.btnSetup);
+            this.panelMenu.Controls.Add(this.btnHome);
+            this.panelMenu.Controls.Add(this.panelSpace);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(230, 561);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSettings.IconChar = FontAwesome.Sharp.IconChar.Cogs;
+            this.btnSettings.IconColor = System.Drawing.Color.Black;
+            this.btnSettings.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSettings.IconSize = 40;
+            this.btnSettings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSettings.Location = new System.Drawing.Point(0, 400);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnSettings.Size = new System.Drawing.Size(230, 60);
+            this.btnSettings.TabIndex = 6;
+            this.btnSettings.Tag = "Settings";
+            this.btnSettings.Text = "Setting";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn5.FlatAppearance.BorderSize = 0;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.IconChar = FontAwesome.Sharp.IconChar.Box;
+            this.btn5.IconColor = System.Drawing.Color.Black;
+            this.btn5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn5.IconSize = 40;
+            this.btn5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn5.Location = new System.Drawing.Point(0, 340);
+            this.btn5.Name = "btn5";
+            this.btn5.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn5.Size = new System.Drawing.Size(230, 60);
+            this.btn5.TabIndex = 5;
+            this.btn5.Tag = "Button5";
+            this.btn5.Text = "Button5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.IconChar = FontAwesome.Sharp.IconChar.Box;
+            this.btn4.IconColor = System.Drawing.Color.Black;
+            this.btn4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn4.IconSize = 40;
+            this.btn4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn4.Location = new System.Drawing.Point(0, 280);
+            this.btn4.Name = "btn4";
+            this.btn4.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn4.Size = new System.Drawing.Size(230, 60);
+            this.btn4.TabIndex = 4;
+            this.btn4.Tag = "Button4";
+            this.btn4.Text = "Button4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDashboard.IconChar = FontAwesome.Sharp.IconChar.TachometerAlt;
+            this.btnDashboard.IconColor = System.Drawing.Color.Black;
+            this.btnDashboard.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 220);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnDashboard.Size = new System.Drawing.Size(230, 60);
+            this.btnDashboard.TabIndex = 3;
+            this.btnDashboard.Tag = "Dashboard";
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetup.FlatAppearance.BorderSize = 0;
+            this.btnSetup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetup.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSetup.IconChar = FontAwesome.Sharp.IconChar.SlidersH;
+            this.btnSetup.IconColor = System.Drawing.Color.Black;
+            this.btnSetup.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSetup.IconSize = 40;
+            this.btnSetup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetup.Location = new System.Drawing.Point(0, 160);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnSetup.Size = new System.Drawing.Size(230, 60);
+            this.btnSetup.TabIndex = 2;
+            this.btnSetup.Tag = "Setup";
+            this.btnSetup.Text = "Setup";
+            this.btnSetup.UseVisualStyleBackColor = true;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHome.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.btnHome.IconColor = System.Drawing.Color.Black;
+            this.btnHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnHome.IconSize = 40;
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(0, 100);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnHome.Size = new System.Drawing.Size(230, 60);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Tag = "Home";
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panelSpace
+            // 
+            this.panelSpace.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSpace.Location = new System.Drawing.Point(0, 80);
+            this.panelSpace.Name = "panelSpace";
+            this.panelSpace.Size = new System.Drawing.Size(230, 20);
+            this.panelSpace.TabIndex = 7;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.btnMenu);
+            this.panelLogo.Controls.Add(this.pboxLogo);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(230, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenu.IconChar = FontAwesome.Sharp.IconChar.Bars;
+            this.btnMenu.IconColor = System.Drawing.Color.Black;
+            this.btnMenu.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMenu.IconSize = 32;
+            this.btnMenu.Location = new System.Drawing.Point(173, 0);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(57, 80);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // pboxLogo
+            // 
+            this.pboxLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pboxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pboxLogo.Image")));
+            this.pboxLogo.Location = new System.Drawing.Point(0, 0);
+            this.pboxLogo.Name = "pboxLogo";
+            this.pboxLogo.Size = new System.Drawing.Size(174, 80);
+            this.pboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pboxLogo.TabIndex = 0;
+            this.pboxLogo.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panelMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OBD Program | HEMA Industy Inc.";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pboxLogo;
+        private FontAwesome.Sharp.IconButton btnSettings;
+        private FontAwesome.Sharp.IconButton btn5;
+        private FontAwesome.Sharp.IconButton btn4;
+        private FontAwesome.Sharp.IconButton btnDashboard;
+        private FontAwesome.Sharp.IconButton btnSetup;
+        private FontAwesome.Sharp.IconButton btnHome;
+        private FontAwesome.Sharp.IconButton btnMenu;
+        private System.Windows.Forms.Panel panelSpace;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,149 @@
+﻿using FontAwesome.Sharp;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Sample_2
+{
+    public partial class Main : Form
+    {
+
+        // Fields
+        private Panel buttonBorderRight;
+        private IconButton currentButton;
+
+        // Constructor
+        public Main()
+        {
+            InitializeComponent();
+            buttonBorderRight = new Panel();
+            buttonBorderRight.Size = new Size(7, 60);
+            panelMenu.Controls.Add(buttonBorderRight);
+            this.DoubleBuffered = true;
+        }
+
+        // Side Menu Button Colors
+        #region
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(229, 27, 36);
+            public static Color color2 = Color.FromArgb(229, 27, 36);
+            public static Color color3 = Color.FromArgb(229, 27, 36);
+            public static Color color4 = Color.FromArgb(229, 27, 36);
+            public static Color color5 = Color.FromArgb(229, 27, 36);
+            public static Color color6 = Color.FromArgb(229, 27, 36);
+        }
+        #endregion
+
+        // Active Side Menu Button Handling
+        #region
+        private void ActiveButton(object senderButton, Color color)
+        {
+            if (senderButton != null)
+            {
+                DeactiveButton();
+                currentButton = (IconButton)senderButton;
+                currentButton.BackColor = Color.FromArgb(199, 200, 201);
+                currentButton.ForeColor = Color.FromArgb(0, 0, 0);
+                currentButton.IconColor = color;
+
+                // Button Border Right
+                buttonBorderRight.BackColor = color;
+                buttonBorderRight.Location = new Point(223, currentButton.Location.Y);
+                buttonBorderRight.Visible = true;
+                buttonBorderRight.BringToFront();
+            }
+        }
+        #endregion
+
+        // Deactive Side Menu Button Handing
+        #region
+        private void DeactiveButton()
+        {
+            if (currentButton != null)
+            {
+                currentButton.BackColor = Color.FromArgb(255, 255, 255);
+                currentButton.ForeColor = Color.FromArgb(0, 0, 0);
+                currentButton.IconColor = Color.FromArgb(0, 0, 0);
+            }
+        }
+        #endregion
+
+        // Menu Collapse / Expand
+        #region
+        private void CollapseMenu()
+        {
+            if (panelMenu.Width >= 230)
+            {
+                panelMenu.Width = 80;
+                pboxLogo.Visible = false;
+                btnMenu.Dock = DockStyle.Fill;
+                btnMenu.IconSize = 28;
+                buttonBorderRight.Visible = false;
+                /**/
+                foreach (IconButton menuButton in panelMenu.Controls.OfType<IconButton>())
+                {
+                    menuButton.Text = "";
+                    menuButton.Padding = new Padding(0);
+                    menuButton.ImageAlign = ContentAlignment.MiddleCenter;
+                    menuButton.IconSize = 32;
+                }
+                /**/
+            }
+            else
+            {
+                panelMenu.Width = 230;
+                pboxLogo.Visible = true;
+                btnMenu.Dock = DockStyle.Right;
+                btnMenu.IconSize = 32;
+                buttonBorderRight.Visible = true;
+                foreach (IconButton menuButton in panelMenu.Controls.OfType<IconButton>())
+                {
+                    menuButton.Text = menuButton.Tag.ToString();
+                    menuButton.Padding = new Padding(20, 0, 0, 0);
+                    menuButton.ImageAlign = ContentAlignment.MiddleLeft;
+                    menuButton.IconSize = 40;
+                }
+            }
+        }
+        #endregion
+
+        // Side Menu Button Click Processes //
+        #region
+        private void btnMenu_Click(object sender, System.EventArgs e)
+        {
+            CollapseMenu();
+        }
+
+        private void btnHome_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color1);
+        }
+
+        private void btnSetup_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color2);
+        }
+
+        private void btnDashboard_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color3);
+        }
+
+        private void btn4_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color4);
+        }
+
+        private void btn5_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color5);
+        }
+
+        private void btnSettings_Click(object sender, System.EventArgs e)
+        {
+            ActiveButton(sender, RGBColors.color6);
+        }
+        #endregion
+    }
+}
